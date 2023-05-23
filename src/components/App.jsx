de88c7dc8a34c4734a40bdbe7745a8d80dde259d
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-05-23 19:27:50 +0300

    fix error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import { Filter } from './Filter/Filter';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import css from './App.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { allContacts, isLoading, error } from './Redux/selectors';
+import { useDispatch } from 'react-redux';
+// import { allContacts, isLoading, error } from './Redux/selectors';
 import { useEffect } from 'react';
 import { fetchContacts } from './Redux/operation/operation';
 import { RegistrationForm } from './forms/registrationForm';
@@ -14,9 +14,9 @@ import { Layout } from './Layout';
 import { Routes, Route } from 'react-router-dom';
 
 export const App = () => {
-  const contacts = useSelector(allContacts);
-  const loading = useSelector(isLoading);
-  const isError = useSelector(error);
+  // const contacts = useSelector(allContacts);
+  // const loading = useSelector(isLoading);
+  // const isError = useSelector(error);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchContacts());
